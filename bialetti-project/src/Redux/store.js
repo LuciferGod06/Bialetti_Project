9735b073569643c28103bb5ba26110eff114f8ab
vinusingh36 <vineet.vinusingh36@gmail.com>
2023-07-06 10:54:22 +0530

    created initial structure of app

---
+++ bialetti-project/src/Redux/store.js
@@ -0,0 +1,8 @@
+import { legacy_createStore as createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+
+const rootReducer = combineReducers({})
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+export { store }
